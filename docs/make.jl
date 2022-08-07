e28a9047cc2d2fff54b954d507303299cd05b66c
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2022-08-07 14:16:19 +0200

    Fixed docs build

--- docs/make.jl
+++ docs/make.jl
@@ -5,15 +5,7 @@ using Plots; pyplot()
 
 makedocs(;
     modules=[Hydrogen],
-    authors="Stefanos Carlström <stefanos.carlstrom@gmail.com> and contributors",
-    repo="https://github.com/JuliaAtoms/Hydrogen.jl/blob/{commit}{path}#L{line}",
     sitename="Hydrogen.jl",
-    format=Documenter.HTML(;
-                           prettyurls=get(ENV, "CI", "false") == "true",
-                           canonical="https://juliatatoms.github.io/Hydrogen.jl",
-                           assets=String["assets/latex.js"],
-                           mathengine = Documenter.MathJax()
-    ),
     pages=[
         "Home" => "index.md",
         "Orbitals" => "orbitals.md",
